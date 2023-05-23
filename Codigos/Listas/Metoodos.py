76cabd5a72e1d9f535389e987562725c09c146f2
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-05-22 23:10:47 -0500

    23 de mayo

---
+++ Codigos/Listas/Metoodos.py
@@ -0,0 +1,50 @@
+"""
+Metodos para lista.
+
+append()
+
+#* Agregar un elemento en el ultimo posicionamiento
+
+clear()
+
+#* Limpia la lista, dejando la lista vacia
+
+extend()
+
+#* Une una lista
+
+count()
+
+#* Cuenta cuantas veces existe un elemento en una lista
+
+
+index()
+
+#* Indica el posicionamiento de un elemento en la lista, el primero en ser encontrado
+
+insert(indice,elemento)
+
+#* Inserta un elemento en la lista, en una posicion definida 
+
+pop()
+
+#* Elimina el ultimo elemento de la lista
+
+remove()
+
+#* Elimina el elemento buscado en una lista, elimina el primero que encuentra.
+
+reverse()
+
+#* Da la vuelta a una lista, el primer elemento hacia el ultimo.
+
+
+sort()
+
+#* Ordena los elementos de una lista, de menor a mayor.
+
+#? sort(reverse = True)
+
+#* Hace lo mismo pero de mayor a menor.
+
+"""
