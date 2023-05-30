09c8f008ac21043678deb4a81df55ce2087587cd
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-05-30 16:10:35 -0500

    Ejercicio 2

--- Codigos/Matrices/AlmacenarDatos.py
+++ Codigos/Matrices/AlmacenarDatos.py
@@ -1,20 +1,35 @@
 import numpy as np
 
+def mayor(arreglo):
+    mayor = 0
+    for i in range(0,len(arreglo)):
+        if mayor < arreglo[i][1]:
+            mayor = arreglo[i][1]
+            indice = i
+    return indice
+
+def menor(arreglo):
+    menor = 100
+    for i in range(0,len(arreglo)):
+        if menor > arreglo[i][1]:
+            menor = arreglo[i][1]
+            indice = i
+    return indice
+
 arreglo = []
 
-for i in range(2):
+for i in range(3):
     arreglo.append([])
-    nombre_ciudad  = str(input(f"Ingrese el numero de la  :"))
-    temp_ciudad  = float(input(f"Ingrese el temperatura de la ciudad :"))
+    nombre_ciudad  = str(input(f"Ingrese el nombre de la ciudad {i+1} :"))
+    temp_ciudad  = float(input(f"Ingrese el temperatura de la ciudad {i+1} :"))
     arreglo[i].append(nombre_ciudad)
-    arreglo[i].append(temp_ciudad)
+    arreglo[i].append(temp_ciudad) 
+print(f"La ciudad con mayor temperatura es : {arreglo[mayor(arreglo)][0]}\n La tempertura es : {arreglo[mayor(arreglo)][1]}")
+print(f"La ciudad con menor temperatura es : {arreglo[menor(arreglo)][0]}\n La tempertura es : {arreglo[menor(arreglo)][1]}")
+
 arreglo = np.array(arreglo)
 print(arreglo)
 
 
-[["Alejandro",18],["Maria",20]]
-
-#? Crear un arreglo que almacene el nombre de la materia y la nota final 
-#? 3 Materias y mostrarlas con el tabulate
 
 
