bfe260b63f3a077f9efa29f662b7dce0b8090f18
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-09-05 09:54:43 -0500

    Clase 2

--- Codigos_2023-2/Codigos_Clase/Clase 1/MostrarPorPantalla.py
+++ Codigos_2023-2/Codigos_Clase/Clase 1/MostrarPorPantalla.py
@@ -1,125 +1,128 @@
-# # """
-# # ! Para mostrar un mensaje en pantalla se debe utilizar la funcion 
+# # # """
+# # # ! Para mostrar un mensaje en pantalla se debe utilizar la funcion 
 
-# # ? print()
-
-# # """
-
-# # print("Hola Mundo")
-
-
-# # """
-
-# # ! Concatenacion 
-
-nombre = "Alejandro"
-
-apellido = "Sierra"
-
-edad = 18
-
-nombre_completo = nombre + " " + apellido
-
-
-print(f"Su nombre es:{nombre_completo} y su edad es : {edad}")
-
-
-
-
-
-
-# palabra = " "
-
-
-# palabra2 = ''
-
-
-# # # Opcion 1: 
-# # #     ? "Hola" +" Mundo"
-# # # Opcion 2: 
-# # #     ? "Alejandro"," Sierra"
-# # # Opcion 3: 
-# # #     ? f"Mi nombre es : {nombre}"
+# # # ? print()
 
 # # # """
 
-# nombre = input("Ingrese su nombre: ")
+# # # print("Hola Mundo")
 
-# edad = input("Ingrese su edad: ")
 
-# # # palabra1 = "Hola + Mundo"
+# # # """
 
-# # # palabra2 = "Alejandro"," Sierra"
+# # # ! Concatenacion 
 
-# palabra3 =  f"Mi nombre es : {nombre} y tengo : {edad} años"
+# nombre = "Alejandro"
 
-# print(palabra3)
+# apellido = "Sierra"
 
-val1 = int(20)
+# edad = 18
 
-val2 = float(2.2)
+# nombre_completo = nombre + " " + apellido
 
-val3 = str("Alejandro")
 
-val4 = bool(True)
+# print(f"Su nombre es:{nombre_completo} y su edad es : {edad}")
 
 
 
 
 
 
+# # palabra = " "
 
 
-# # # suma = 2 + 2
-# # # # > 4 
+# # palabra2 = ''
 
 
-# # # resta = 2 -2
-# # # # > 0
+# # # # Opcion 1: 
+# # # #     ? "Hola" +" Mundo"
+# # # # Opcion 2: 
+# # # #     ? "Alejandro"," Sierra"
+# # # # Opcion 3: 
+# # # #     ? f"Mi nombre es : {nombre}"
 
-# # # multiplicacion = 2 * 2
-# # # # > 4
+# # # # """
+
+# # nombre = input("Ingrese su nombre: ")
+
+# # edad = input("Ingrese su edad: ")
+
+# # # # palabra1 = "Hola + Mundo"
+
+# # # # palabra2 = "Alejandro"," Sierra"
+
+# # palabra3 =  f"Mi nombre es : {nombre} y tengo : {edad} años"
+
+# # print(palabra3)
+
+# val1 = int(20)
+
+# val2 = float(2.2)
+
+# val3 = str("Alejandro")
+
+# val4 = bool(True)
 
 
-# # # #Exacta
 
-# # # division = 2 // 3 
 
-# # # # > 0 
 
-# # # #Inexacta
 
-# # # division = 2 // 3 
 
-# # # # > 0.666
 
-# # # modulo = 2 % 5
+# # # # suma = 2 + 2
+# # # # # > 4 
 
-# # # # > 2
 
-# # # potencia = 2 ** 3
+# # # # resta = 2 -2
+# # # # # > 0
 
-# # # # > 8
+# # # # multiplicacion = 2 * 2
+# # # # # > 4
+
+
+# # # # #Exacta
+
+# # # # division = 2 // 3 
+
+# # # # # > 0 
+
+# # # # #Inexacta
+
+# # # # division = 2 // 3 
+
+# # # # # > 0.666
+
+# # # # modulo = 2 % 5
+
+# # # # # > 2
+
+# # # # potencia = 2 ** 3
+
+# # # # # > 8
  
 
-"""
+# """
     
     
-    Realizar un programa que solicite 
-    al usuario la cantidad de kilometros 
-    que recorre su vehiculo y la cantidad de
-    litros de combustible que consume durante
-    el recorrido. 
-    Mostrar el consumo de combustible por kilometro
+#     Realizar un programa que solicite 
+#     al usuario la cantidad de kilometros 
+#     que recorre su vehiculo y la cantidad de
+#     litros de combustible que consume durante
+#     el recorrido. 
+#     Mostrar el consumo de combustible por kilometro
 
 
-    Ejemplo:
-    Kilometros recorrido: 260
-    Litros consumidos: 12.5
-    El consume por Kilometro es 20.8
+#     Ejemplo:
+#     Kilometros recorrido: 260
+#     Litros consumidos: 12.5
+#     El consume por Kilometro es 20.8
     
 
-"""
+# """
 
-
+nombre = "nombre"
+tipo_de_vehículo = "tipo_de_vehículo"
+horas = "horas"
+print(f"Recibo de Pago \n \tLa Persona :{nombre} ocupo el servicio de parqueadero {tipo_de_vehículo} durante {horas} horas. \nFeliz dia.	")
 
