38e4a87c45a05198d7f0992707309638e8b55404
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-09-26 14:06:14 -0500

    Clase 6

--- Codigos_2023-2/Codigos_Clase/Clase 6/Funciones.py
+++ Codigos_2023-2/Codigos_Clase/Clase 6/Funciones.py
@@ -1,28 +1,28 @@
-"""
-Sumatorias == > 1+2+3+4+5+6+....+100
-n(n+1)/2
+# """
+# Sumatorias == > 1+2+3+4+5+6+....+100
+# n(n+1)/2
 
-"""
-"""
-Factorial ->
-
-
-Factorial 5 
-
-1*2*3*4*5 = 120
+# """
+# """
+# Factorial ->
 
 
+# Factorial 5 
 
-"""
-def factorial(numero):
-    if numero == 1:
-        print(numero, end= " = ")
-        return 1
-    else:
-        print(numero, end= " * ")
-        return factorial(numero-1) * numero
+# 1*2*3*4*5 = 120
+
+
+
+# """
+# def factorial(numero):
+#     if numero == 1:
+#         print(numero, end= " = ")
+#         return 1
+#     else:
+#         print(numero, end= " * ")
+#         return factorial(numero-1) * numero
     
-# print(factorial(5))
+# # print(factorial(5))
 
 
 
@@ -32,31 +32,47 @@ def factorial(numero):
 
 
 
-# Numero  = 10
-def MultiplicacionExpresion(numero):
-    # Caso base
-    if numero == 1:
-        return "1"
-    # Caso Recursivo 
-    else:
-        return f"{MultiplicacionExpresion(numero - 1)} * {numero}"
-def Factorial(numero):
-    # Caso base
-    if numero == 1:
+# # Numero  = 10
+# def MultiplicacionExpresion(numero):
+#     # Caso base
+#     if numero == 1:
+#         return "1"
+#     # Caso Recursivo 
+#     else:
+#         return f"{MultiplicacionExpresion(numero - 1)} * {numero}"
+# def Factorial(numero):
+#     # Caso base
+#     if numero == 1:
+#         return 1
+#     # Caso Recursivo 
+#     else:
+#         resultado = Factorial(numero - 1)
+#         return numero * resultado
+
+
+# if __name__ == '__main__':
+#     print(f"{MultiplicacionExpresion(7)} = {Factorial(7)}")
+
+
+
+
+
+# # # 10 * 9 * 8 * 7 
+
+# # # 1 * 2 * 3 * 4 * 5 * 6
+
+
+
+def Collazt(num):
+    if num == 1:
         return 1
-    # Caso Recursivo 
     else:
-        resultado = Factorial(numero - 1)
-        return numero * resultado
-
-
-if __name__ == '__main__':
-    print(f"{MultiplicacionExpresion(7)} = {Factorial(7)}")
-
-
-
-
-
-# # 10 * 9 * 8 * 7 
-
-# # 1 * 2 * 3 * 4 * 5 * 6
+        if(num%2 == 0):
+            return Collazt(num/2)            
+        else:
+            return Collazt(num*3+1)
+    
+print(Collazt(120))
+print(Collazt(20))
+print(Collazt(10))
+            
