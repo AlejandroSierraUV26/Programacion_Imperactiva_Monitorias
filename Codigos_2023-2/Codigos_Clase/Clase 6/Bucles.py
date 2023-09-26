c1eb6f5cedcf5250af65f4a15191a77cfb9d85df
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-09-26 14:56:02 -0500

    Bucles

---
+++ Codigos_2023-2/Codigos_Clase/Clase 6/Bucles.py
@@ -0,0 +1,33 @@
+"""
+min = 1
+max = 10
+
+>>> 1,2,3,4,5,6,7,8,9,10
+
+
+"""
+# n = 10
+# for i in range(1,n+1):
+#     if (i == n):
+#         print(i,end ="")
+#     else:
+#         print(i, end =",")
+
+
+
+
+for num in range(10):
+    if num == 5:
+        continue
+    print(f"El número actual es {num}")
+
+
+"""
+Imprimir los numeros del 1 al 50. pero excluyendo 
+los que son divisibles por 5 y 7
+1,2,3,4,6,8,9,11,12,13,16
+
+"""
+    
+    
+
