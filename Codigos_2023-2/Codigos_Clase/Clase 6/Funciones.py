6e6c48434c70c412f7e3acf4e1ebd162d4c69e48
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-09-26 13:43:40 -0500

    Clase 6

---
+++ Codigos_2023-2/Codigos_Clase/Clase 6/Funciones.py
@@ -0,0 +1,62 @@
+"""
+Sumatorias == > 1+2+3+4+5+6+....+100
+n(n+1)/2
+
+"""
+"""
+Factorial ->
+
+
+Factorial 5 
+
+1*2*3*4*5 = 120
+
+
+
+"""
+def factorial(numero):
+    if numero == 1:
+        print(numero, end= " = ")
+        return 1
+    else:
+        print(numero, end= " * ")
+        return factorial(numero-1) * numero
+    
+# print(factorial(5))
+
+
+
+
+
+
+
+
+
+# Numero  = 10
+def MultiplicacionExpresion(numero):
+    # Caso base
+    if numero == 1:
+        return "1"
+    # Caso Recursivo 
+    else:
+        return f"{MultiplicacionExpresion(numero - 1)} * {numero}"
+def Factorial(numero):
+    # Caso base
+    if numero == 1:
+        return 1
+    # Caso Recursivo 
+    else:
+        resultado = Factorial(numero - 1)
+        return numero * resultado
+
+
+if __name__ == '__main__':
+    print(f"{MultiplicacionExpresion(7)} = {Factorial(7)}")
+
+
+
+
+
+# # 10 * 9 * 8 * 7 
+
+# # 1 * 2 * 3 * 4 * 5 * 6
