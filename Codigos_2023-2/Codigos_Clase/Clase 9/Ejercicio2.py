543e91e323f43d2ac0534fee84e8fb7f0b6ae4fa
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-10-10 11:40:02 -0500

    Clase 9

--- Codigos_2023-2/Codigos_Clase/Clase 9/Ejercicio2.py
+++ Codigos_2023-2/Codigos_Clase/Clase 9/Ejercicio2.py
@@ -7,5 +7,11 @@ Decimal numerador y entero denominador.
 
 
 """
-
-
+while(True):
+    try: 
+        a = int(input("Ingrese  un numero : "))
+        b = int(input("Ingrese  un numero : "))
+        print(a/b)
+        break
+    except Exception as e: 
+        print("Invalid",e)
