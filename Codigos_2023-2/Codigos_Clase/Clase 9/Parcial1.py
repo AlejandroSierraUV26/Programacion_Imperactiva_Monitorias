e622650e986f757abfa5235be6f0d77f94cd78ef
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-10-20 10:53:21 -0500

    Clase 10

---
+++ Codigos_2023-2/Codigos_Clase/Clase 9/Parcial1.py
@@ -0,0 +1,89 @@
+# # def funcion(num):
+# #     if num == 15: 
+# #         return 1
+# #     elif num == 16:
+# #         return 2
+# #     else:
+# #         return 2 + funcion(num-1) + funcion(num-2)
+
+
+# # """
+
+
+# # Resultado = 2 + 16 + 9 = 27
+
+# # funcion(19) = 2 + 9 + 5
+# # funcion(18) = 2 + 5 + 2 
+# # funcion(18) = 2 + 5 + 2
+# # funcion(17) = 2 + 2 + 1
+# # funcion(17) = 2 + 2 + 1
+# # funcion(16) = 2  
+# # funcion(16) = 2
+# # funcion(16) = 2
+# # funcion(16) = 2
+# # funcion(15) = 1
+# # funcion(15) = 1
+ 
+# # """
+# # print(funcion(20))
+
+
+
+
+# def cal (genero):
+#     if genero == "M":
+#         cmc = (
+#                 (13.75 * weight) 
+#                + (5 * height) 
+#                - (6.76 * age) 
+#                + 66)
+#         return (f"The minimum amount of calories is: {cmc}")
+#     elif genero == "F":
+#          cmc = (
+#                 (9.56 * weight) 
+#                 + (1.85 * height) 
+#                 - (4.68* age) 
+#                 + 655)
+#          return (f"The minimum amount of calories is: {cmc}")
+#     else:
+#         return ("The entered gender is incorrect")
+    
+# if __name__ == '__main__':
+#     gender = input("please type your gender (M) if male (F) if female : ")
+#     weight =  float(input("please enter your weight in kilograms: "))
+#     height = float(input("please enter your height in Centimeters: "))
+#     age = int(input("please enter your age: "))
+
+#     print(cal(gender))
+
+
+
+
+
+a = 0
+while (a < 3):
+    print("Hola Mundo")
+    a+=1
+    
+print()
+for i in range(0,3,1):
+    print("Hola Mundo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
