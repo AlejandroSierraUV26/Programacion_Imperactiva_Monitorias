129da7bdd8072bc16e789de2cabc477965540d19
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-11-21 14:52:18 -0500

    Clase 15

--- Codigos_2023-2/Codigos_Clase/Clase 15/Ejemplo3.py
+++ Codigos_2023-2/Codigos_Clase/Clase 15/Ejemplo3.py
@@ -1,26 +1,29 @@
 matrix = [
-    
 ]
 
-"""
-Para 3 materias : 
+def agregar_materia():
+    global matrix
+    nombre = str(input("Ingrese el nombre de la materia : "))
+    matrix.append([])
+    matrix[-1].append(nombre)
+    for i in range(0,3):
+        nota = float(input(f"Ingrese la nota {i+1} : "))
+        matrix[-1].append(nota)
+    print("Materia agregada con exito")
+    
+def promedio(matrix, i):
+    return(matrix[i][1] * 0.4) +(matrix[i][2] * 0.3) + (matrix[i][3] * 0.3)
 
+def mostrar_Datos():    
+    for i in range(0,3):
+        agregar_materia()
+    for i in range(0,3):
+        print(f"{matrix[i][0]} {i+1}: ")
+        print(promedio(matrix,i))
+        
+if __name__ == "__main__":
+    mostrar_Datos()
+    print(matrix)
 
-Para una sola Materia =>
-
-Nota 1 : Parcial I 40% 
-
-Nota 2 : Parcial II 30% 
-
-Nota 3 : Proyecto Final 30% 
-
-
-Nombre_Materia  / Nota_Definitiva / Nota 1 / Nota 2 / Nota 3 /
-    -                   -            -         -        -     
-
-
-
-
-
-
-"""
+    
+    
