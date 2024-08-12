1536615a977de12b4ee56b3c9801e1d4718a1ee3
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2024-08-12 17:48:39 -0500

    Inicio 2024-2

---
+++ Codigos_2024-2/Codigo QR/CodigoQR.py
@@ -0,0 +1,26 @@
+import qrcode
+
+# Grupo 51 -> https://chat.whatsapp.com/Kf5VcxbSYwu5jDZtbnozP0
+# Grupo 52 -> https://chat.whatsapp.com/D7bQ2fRvYVSBWnsEDIZLb0
+
+
+def generar_qr_desde_link(link,nombre_codigo):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    qr_code = qr.make_image(fill_color="black", back_color="white")
+    qr_code.save(fr"Codigo QR\img\{nombre_codigo}.png")
+    
+    
+
+if __name__ == "__main__":
+    enlace = input("Ingresa el enlace: ")
+    nombre_codigo = input("Ingresa el titulo del código QR: ")
+    generar_qr_desde_link(enlace,nombre_codigo)
+
+    print("Se ha generado el código QR exitosamente.")
